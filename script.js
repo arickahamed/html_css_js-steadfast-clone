039e5b379b37163cbc4b90b085525f13523021f2
arick <arickahamed700@gmail.com>
2024-10-04 10:36:35 +0600

    alhamdulillah the mighty html css js done

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("toggledPart");
+const body = document.querySelector("body");
+
+window.onscroll = function() {
+    const header = document.getElementById("header");
+
+    if(window.scrollY > 50) {
+        header.classList.add("scrolled");
+    }else {
+        header.classList.remove("scrolled");
+    };
+};
+
+const handleClick = () => {
+    console.log("Clicked the hamburger menu");
+    sidebar.classList.toggle("open");
+    body.classList.add("no-scroll");
+};
+
+hamburger.addEventListener("click", handleClick);
